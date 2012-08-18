4cbdff17354871543ab4ba6995f30a3cee998f53
Chris Paul <cmpaul@gmail.com>
2012-08-17 18:17:42 -0700

    Initial commit.

---
+++ src/main/resources/archetype-resources/alfresco-amp/src/test/java/ThisIsATest.java
@@ -0,0 +1,13 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.junit.Test;
+
+public class ThisIsATest {
+    @Test
+    public void testThis() {
+        System.out.println("Running some tests...");
+    }
+}
